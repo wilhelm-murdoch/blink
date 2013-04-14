f7c1828754212594d3717b24d9542c3f5d24452c
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2013-04-15 01:40:44 +1000

    fleshed out a few tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,14 +4,30 @@ import blink
 import unittest
 
 class BlinkTest(unittest.TestCase):
+    def setUp(self):
+        self._blink = blink.Blink()
+        self._test = lambda: None
+
     def test_bind_with_callback(self):
-        pass
+        self._blink.bind('test', self._test)
+        self.assertIn('test', self._blink._events)
+        self.assertEqual(len(self._blink._events), 1)
+        self._blink.bind('test', self._test)
+        self.assertIn(self._test, self._blink._events['test'])
+        self.assertEqual(len(self._blink._events['test']), 2)
 
     def test_bind_without_callback(self):
-        pass
+        self._blink.bind('test')(self._test)
+        self.assertIn('test', self._blink._events)
+        self.assertEqual(len(self._blink._events), 1)
+        self._blink.bind('test')(self._test)
+        self.assertIn(self._test, self._blink._events['test'])
+        self.assertEqual(len(self._blink._events['test']), 2)
 
     def test_unbind_known_event(self):
-        pass
+        self._blink.bind('test')(self._test)
+        self._blink.unbind('test')
+        self.assertEqual(self._blink._events, {})
 
     def test_bind_with_arbitrary_arguments(self):
         pass
